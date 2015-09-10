5f0bd7ef419192566a86fd1db361b39d26f4e3dd
david-frederick <david.frederick@gatech.edu>
2015-09-10 05:45:41 -0400

    created a list of users

--- app/app.js
+++ app/app.js
@@ -1,12 +1,32 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.
+  when('/users/:userId', {templateUrl: '/albums/albums.html', controller: 'AlbumController'}).
+  otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller('UserController', ['$http', function($http){
+	var userCtrl = this;
+	userCtrl.users = [];
+
+	$http.get('http://jsonplaceholder.typicode.com/users').success(function(data){
+		userCtrl.users = data;
+	});
+}]);
+
+app.controller('AlbumController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams){
+	var albumCtrl = this;
+	albumCtrl.albums = [];
+
+	$http.get('http://jsonplaceholder.typicode.com/albums').success(function(data){
+		albumCtrl.albums = data;
+	});
 }]);
